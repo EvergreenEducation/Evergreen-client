3a558b04f11a4171da62ca5b702a8de2c9a5f964
do-kevin <do.kevin.vo@gmail.com>
2020-06-17 18:34:45 -0700

    Adds cost data to ExpenseEarningChart

--- src/components/student/ExpenseEarningChart.js
+++ src/components/student/ExpenseEarningChart.js
@@ -3,17 +3,20 @@ import { Line } from 'react-chartjs-2';
 import { each, head } from 'lodash';
 
 export default function (props) {
-  const { pathway, className, earningByGroup } = props;
+  const { pathway, className, earningByGroup, costByGroup } = props;
   const { group_sort_order = [] } = pathway;
 
   const years = [];
 
   const yData = [];
+  const yCostData = [];
 
   each(group_sort_order, (groupName, index) => {
     const totalPay = earningByGroup[groupName];
+    const totalCost = costByGroup[groupName];
     years.push(`Year ${index + 1}`);
     yData.push(totalPay);
+    yCostData.push(totalCost);
   });
 
   const data = {
@@ -39,6 +42,27 @@ export default function (props) {
         pointRadius: 1,
         pointHitRadius: 10,
         data: yData,
+      },
+      {
+        label: 'Cost',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(255, 107, 134,0.4)',
+        borderColor: 'rgba(255, 107, 134,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(255, 107, 134,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(255, 107, 134,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: yCostData,
       },
     ],
   };
